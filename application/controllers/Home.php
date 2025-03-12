a86171a2f35384144b5bc3a51b71c64cdf8d462a
Enrique Rendhi Julian Mahardhika <enrique.22031@mhs.unesa.ac.id>
2025-03-12 10:55:17 +0700

    perubahan logo mata

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -5,7 +5,7 @@ class Home extends CI_Controller {
 	public function index()
 	{
         $this->load->view('header');
-        // $this->load->view('bpjs');
+        $this->load->view('bpjs');
 		$this->load->view('layanan');
 		$this->load->view('about');
 		$this->load->view('background');
