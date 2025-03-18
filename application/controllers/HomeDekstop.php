40c4b08ad006b424a4e2ded08dd82d15082c757d
zainul_16 <zainul@keraton.co.id>
2025-03-18 15:53:58 +0700

    disable mobile detection

---
+++ application/controllers/HomeDekstop.php
@@ -0,0 +1,26 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class HomeDekstop extends CI_Controller {
+	public function __construct() {
+        parent::__construct();
+		// if (detect_device_and_redirect()) {
+		// 	redirect('home_mobile');
+		// }
+    }
+
+	public function index()
+	{
+		$this->load->view('dekstop/popup');
+        $this->load->view('dekstop/header');
+        $this->load->view('dekstop/bpjs');
+		$this->load->view('dekstop/layanan');
+		$this->load->view('dekstop/about');
+		$this->load->view('dekstop/background');
+        $this->load->view('dekstop/footer');
+	}
+
+	public function test() {
+		$this->load->view('dekstop/test');
+	}
+}
